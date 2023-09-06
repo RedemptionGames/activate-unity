37e379fa289f69452855d7dd48a6f4c2993a326a
rbehshad <rbehshad@redemptiongames.com>
2023-09-06 12:29:18 -0700

    Update stdout check for successful activation

--- src/unity.js
+++ src/unity.js
@@ -5,8 +5,8 @@ module.exports = { createManualActivationFile, activateManualLicense, activateSe
 
 async function activateSerialLicense(unityPath, username, password, serial) {
     // use '-projectPath ?' for skipping project indexing
-    const stdout = await executeUnity(unityPath, `-batchmode -nographics -quit -logFile "-" -username "${username}" -password "${password}" -serial "${serial}"`);
-    if (!stdout.includes('Next license update check is after')) {
+    const stdout = await executeUnity(unityPath, `-batchmode -nographics -quit -logFile "-" -projectPath "?" -username "${username}" -password "${password}" -serial "${serial}"`);
+    if (!stdout.includes('[Licensing::Module] Serial number assigned to:')) {
         throw new Error('Activation failed');
     }
 }
@@ -19,7 +19,7 @@ async function createManualActivationFile(unityPath) {
 async function activateManualLicense(unityPath, licenseData) {
     fs.writeFileSync('license.ulf', licenseData);
     const stdout = await executeUnity(unityPath, `-batchmode -nographics -quit -logFile "-" -manualLicenseFile license.ulf`);
-    if (!stdout.includes('Next license update check is after')) {
+    if (!stdout.includes('[Licensing::Module] Serial number assigned to:')) {
         throw new Error('Activation failed');
     }
 }
